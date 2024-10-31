9304a83e0370c0a0607f7eab8d94d563f5994d2d
Clint Jones <clinton.alan.jones@gmail.com>
2024-10-31 09:53:16 -0700

    Finished lesson 4.2

--- lessons/4.2/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
+++ lessons/4.2/18-Stu_Event-Bubbling/Unsolved/assets/js/script.js
@@ -1,11 +1,12 @@
-// TODO: Which element is the following line of code selecting?
+// set a variable by selecting the div containing the image and direction buttons
 const carousel = document.querySelector('.carouselbox');
-// TODO: Which element is the following line of code selecting?
+// set variables by selecting the next and previous buttons
 const next = carousel.querySelector('.next');
 const prev = carousel.querySelector('.prev');
 let index = 0;
 let currentImage;
 
+// array holding the available carouselbox background images
 const images = [
   'https://picsum.photos/id/10/300/200',
   'https://picsum.photos/id/20/300/201',
@@ -13,38 +14,61 @@ const images = [
   'https://picsum.photos/id/47/300/203',
 ];
 
+// set the initial carouselbox background image to the first in the array
 carousel.style.backgroundImage = "url('https://picsum.photos/id/10/300/200')";
 
 function navigate(direction) {
-  index = index + direction;
+  index = index + direction;  // index is increased or decreased by 1
+  // handle wraparound for 'previous on first image' and 'next on last image'
   if (index < 0) {
+    // index was on the first image: previous (-1) means set it to the last image (array index 3)
     index = images.length - 1;
   } else if (index > images.length - 1) {
+    // index was on he last image: next image (+1) means 'wrap around' to the first image (array index 0)
     index = 0;
   }
+  // the previous/next was still within array range so go ahead and assign it
   currentImage = images[index];
+  // update the carouselbox background image to reflect the direction clicked
   carousel.style.backgroundImage = `url('${currentImage}')`;
 }
 
-// TODO: Describe the functionality of the following event listener.
+// Prevent a the browser from opening the background image itself
+//    if the user clicks the background -- this explicitly sets
+//    the click action to set the image for the current window
 carousel.addEventListener('click', function () {
   window.location.href = images[index];
 });
 
-// TODO: Describe the functionality of the following event listener.
+// handle a Next button click to advance the count forward with a +1 index
 next.addEventListener('click', function (event) {
-  // TODO: What is the purpose of the following line of code?
+  // Prevent a the browser from opening the next background image itself
   event.stopPropagation();
 
   navigate(1);
 });
 
-// TODO: Describe the functionality of the following event listener.
+// handle a Previous button click to advance the count backward with a -1 index
 prev.addEventListener('click', function (event) {
-  // TODO: What would happen if we didn't add the following line of code?
+  // Prevent a the browser from opening the last background image itself
   event.stopPropagation();
 
   navigate(-1);
 });
 
+// set the initial background to the first image
 navigate(0);
+
+// BONUS:
+// ref: https://gomakethings.com/whats-the-difference-between-javascript-event-delegation-bubbling-and-capturing/
+/*
+  Event delegation: designate a parent element as the listener for 
+     all of the events that happen inside it (down the DOM heirarchy)
+
+  Event bubbling: events bubble up to the parent (up the heirarchy), 
+     then on to any containers or divs where the element was embedded, 
+     then on to the body, ...
+     ... then the html element
+     ... then the document
+     ... then the window.
+  */
