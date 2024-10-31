2e2a70294f20258d3aec1a2d4d3d4c5d09057993
Clint Jones <clinton.alan.jones@gmail.com>
2024-10-31 09:31:14 -0700

    finished lesson 4.2

--- lessons/4.2/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ lessons/4.2/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -8,5 +8,21 @@ function setCounterText() {
 }
 
 // TODO: Add event listener to increment button
+incrementEl.addEventListener('click', function() {
+  count++;
+  setCounterText();
+});
 
 // TODO: Add event listener to decrement button
+decrementEl.addEventListener('click', function() {
+  if(0 < count) {
+    count--;
+  }
+
+  setCounterText();
+});
+
+
+// BONUS: other DOM events:
+// ref https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/events
+// removeEventListener(), hasListener();
--- lessons/4.2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ lessons/4.2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -3,5 +3,14 @@ const container = document.querySelector('.container');
 container.addEventListener('click', function (event) {
   const element = event.target;
 
-  // TODO: Complete function
+  // set the div box text to the element's data-number value
+  element.textContent = element.dataset.number;
+  
+  // set the state to visible
+  element.setAttribute('data-state', 'visible');
+  // alternatively: element.dataset.state = 'visible';
 });
+
+// BONUS:
+// getElementById, getElementsByClassName, getElementsByName, getElementsByTagName, querySelector, querySelectorAll
+// removeAttribute, toggleAttribute
