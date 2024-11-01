6d7147863815ac820ad150ab4fac3b8ea1706399
Clint Jones <clinton.alan.jones@gmail.com>
2024-11-01 13:30:59 -0700

    basic functionality light and dark mode

--- challenges/module4/assets/js/blog.js
+++ challenges/module4/assets/js/blog.js
@@ -1,5 +1,6 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
-
+const mainEl= document.querySelector('main');
+const btnBackEl = document.querySelector('#back');
 // TODO: Create a function that builds an element and appends it to the DOM
 
 // TODO: Create a function that handles the case where there are no blog posts to display
--- challenges/module4/assets/js/logic.js
+++ challenges/module4/assets/js/logic.js
@@ -1,5 +1,27 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+const bodyEl = document.querySelector("body");
+const btnMode = document.querySelector("#toggle");
 
+let lightMode = localStorage.getItem('lightMode');
+
+function toggleDarkMode() {
+  if(! lightMode) {
+    lightMode = 'light' // set a default if not available from local storage
+  }
+
+  if( 'light' === lightMode.toLowerCase() ) {
+    lightMode = 'dark';
+  } else {
+    lightMode = 'light';
+  }
+
+  bodyEl.setAttribute('class', lightMode);
+  localStorage.setItem('lightMode', lightMode);
+  
+  return;
+}
+
+btnMode.addEventListener('click', toggleDarkMode);
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 
