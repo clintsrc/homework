3bec9a409896a6445c059c52cfa410c978e3490a
Clint Jones <clinton.alan.jones@gmail.com>
2024-11-21 13:52:02 -0800

    Prep for 7.3 and mini project

---
+++ lessons/7.3/22-Stu_Colors/Unsolved/index.js
@@ -0,0 +1,32 @@
+/*
+Work with a partner to implement the following user story:
+As a developer, I want to initialize my project and create a package.json file.
+As a developer, I want to use ESM style imports in my project.
+As a developer, I want to be able to save packages to my project.
+As a developer, I want to install and use the colors npm package.
+
+Acceptance Criteria
+It's done when I see a package.json file created.
+It's done when I set the "type" property to "module" in my package.json to allow for ESM style imports.
+It's done when I have installed the colors package successfully and see it listed in the package.json file.
+It's done when I type node index.js Sally 500 and the following is logged, with Sally highlighted with a bright yellow background and 500 highlighted with a bright green background, since the profit is a number greater than 0:
+      My name is Sally and I made 500 dollars this quarter.
+It's done when I type node index.js Bob -300 and the following is logged with Bob highlighted with a bright yellow background and -300 highlighted with a bright red background since the profit is a number less than or equal to 0:
+My name is Bob and I made -300 dollars this quarter.
+
+📝 Notes
+Refer to the documentation:
+   colors
+Note: In the documentation, there will be two ways to use the colors package. We will be using the option that does not extend the String prototype. We will learn more about prototypes later.
+
+💡 Hints
+Remember that we can access command line arguments using process.argv.
+
+🏆 Bonus
+Q: What is a String prototype?
+A: TODO
+ 
+ *
+ */
+
+
---
+++ lessons/7.3/24-Stu_Basic-Inquirer/Unsolved/index.js
@@ -0,0 +1,22 @@
+/*
+Work with a partner to implement the following user story:
+As a developer, I want to create a command-line application that takes in input from the user and logs a message with a selected color to the console.
+
+Acceptance Criteria
+It's done when I have initialized my repository with a package.json file by running npm init -y.
+It's done when I set the "type" property to "module" in my package.json to allow for ESM style imports.
+It's done when I have installed inquirer added it to my list of dependencies by running npm i inquirer.
+It's done when I have installed colors and added it to my list of dependencies by running npm i colors.
+It's done when the application asks the user, "What text would you like to log?".
+It's done when the application asks the user, "What color would you like to use?", and has the options "red", "blue", "green", "yellow", "cyan", and "magenta".
+It's done when the text is logged in the selected color.
+
+💡 Hints
+Use the inquirer npm page to see how to use lists and inputs.
+
+🏆 Bonus
+Q: How could you also add some styling choices to the text logged using colors and inquirer?
+A: TODO
+*/
+
+
