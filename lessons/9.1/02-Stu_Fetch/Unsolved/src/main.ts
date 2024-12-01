77abb16d6e513eb09f7bd15765cf3b4519e5461c
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-01 12:45:19 -0800

    latest 9.1 lessons

--- lessons/9.1/02-Stu_Fetch/Unsolved/src/main.ts
+++ lessons/9.1/02-Stu_Fetch/Unsolved/src/main.ts
@@ -1,3 +1,37 @@
+/* 
+ *
+
+  Implement the Fetch API to Display GitHub Data as HTML
+  Work with a partner to implement the following user story:
+
+  As a manager, I want an app that calls the GitHub API so that I can print a list of my team's GitHub usernames and links to their profiles.
+
+  Acceptance Criteria
+  It's done when I press the "Click Me" button, and a list of five GitHub users appears on the page underneath the button.
+  It's done when each user includes their login name and a URL to their GitHub profile.
+  It's done when I click on a user, and a new tab opens to their GitHub profile.
+
+  💡 Hints
+  How can we compile TypeScript and HTML together?
+
+  🏆 Bonus
+  If you've completed this activity, work through the following challenge with your partner to further your knowledge:
+  Q: What are fetch options used for?
+  A: Fetch options are used for API requests (e.g. to use http method like GET, POST, PUT, DELETE and credentials)
+    and error handling for the response
+  Q: What tsconfig options did Vite create? Are there options that you've used in the past that are missing?
+  A: Vite manages the typescript project (tsconfig.json). I'm not sure, but if Vite was used for this lesson's starter 
+     code it probably set these options:
+      "moduleResolution": "Bundler" handles module bundling and resolution.
+      "target": "ES2020", "module": "ESNext" to use the latest language features
+      "rootDir": "src" specifies that the source files are located in the src directory.
+      "outDir": "dist" sets the compiled output are in the dist directory
+      "strict": true helps with type safety
+
+*
+*/
+
+
 // ! When dynamically generating elements in TypeScript, we need to cast the element to the correct type
 const usersContainer = document.getElementById('users') as HTMLDivElement;
 const fetchButton = document.getElementById(
@@ -6,11 +40,30 @@ const fetchButton = document.getElementById(
 
 const REQUEST_URL = 'https://api.github.com/users?per_page=5';
 
-const getUsers = () => {
-  // TODO: Add a fetch call to the GitHub API
-  // TODO: Then parse the JSON response
-  // TODO: Then iterate over the data and generate your HTML
+const getUsers = async () => {
+
+  try {
+    // TODO: Add a fetch call to the GitHub API
+    const response = await fetch(REQUEST_URL);
+
+    // TODO: Then parse the JSON response
+    const userData = await response.json();
+
+    // Clear previous users
+    usersContainer.innerHTML = '';
+
+    // TODO: Then iterate over the data and generate your HTML
+    // extract the user login name (login) their GitHub profile url (html_url)
+    userData.forEach((user: { login: string; html_url: string }) => {
+      const userButton = createUserButton(user.login, user.html_url);
+      usersContainer.appendChild(userButton);
+    });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
 };
+
+fetchButton.addEventListener('click', getUsers);
 
 const createUserButton = (username: string, url: string) => {
   // Creating a h3 element and a p element
--- lessons/9.1/04-Stu_Deconstruct-Parameters/Unsolved/src/main.ts
+++ lessons/9.1/04-Stu_Deconstruct-Parameters/Unsolved/src/main.ts
@@ -1,11 +1,37 @@
+/* 
+ *
+
+  Add Comments to Implementation of API Parameters
+  Work with a partner to add comments describing the functionality of the code found in Unsolved/src/main.ts.
+
+  📝 Notes
+  Refer to the documentation: 
+  https://docs.github.com/en/rest/using-the-rest-api/getting-started-with-the-rest-api?apiVersion=2022-11-28
+
+  🏆 Bonus
+  Q: What are query strings, and how are they used to create parameters for APIs?
+  A: Query strings are additional information included in a URL which is used for API parameters 
+     to filter results similar to a database query. A query string begins with the ? character
+     followed by one or more key value pairs separated by & (e.g. ?name=bob&age=30)
+  Q: What is the difference between HTTP and HTTPS? How does this difference affect our HTTP requests? 
+  A: HTTP is insecure unencrypted web data transfer. HTTPS uses TLS encryption with a certificate and
+     so any sensitive data, including credentials are transferred securely.
+
+*
+*/
+
 import './style.css';
 
 // Explain each parameter in comments below.
 const REQUEST_URL =
   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc';
 
+// fetch runs an HTTP GET (default) of the github REST api URL 
 fetch(REQUEST_URL)
+  // the the server responds with the requested data in json format
+  //    which is parsed here
   .then((response) => response.json())
+  // the json result is displayed in the web client console
   .then((data) => {
     console.log(data);
   });
