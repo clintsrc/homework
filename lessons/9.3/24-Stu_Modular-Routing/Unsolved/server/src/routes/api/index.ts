f5a020d24774efe14e42dcdb488f2f196e6fb3fd
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-11 20:58:22 +0000

    Fixed 9.3.24 api routes adjusted after adding middleware

--- lessons/9.3/24-Stu_Modular-Routing/Unsolved/server/src/routes/api/index.ts
+++ lessons/9.3/24-Stu_Modular-Routing/Unsolved/server/src/routes/api/index.ts
@@ -1,7 +1,9 @@
 import feedbackRoutes from './feedbackRoutes.js';
+import tipsRoutes from './tipsRoutes.js';
 import express from 'express';
 const router = express.Router();
 
 router.use('/feedback', feedbackRoutes);
+router.use('/tips', tipsRoutes);    // add the tips routes to middleware
 
 export default router;
--- lessons/9.3/24-Stu_Modular-Routing/Unsolved/server/src/routes/api/tipsRoutes.ts
+++ lessons/9.3/24-Stu_Modular-Routing/Unsolved/server/src/routes/api/tipsRoutes.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 import TipService from '../../service/TipService.js';
 const router = express.Router();
+
+
+// FIX: since the middleware in the index is loading this GET route to 
+//  path /tips, the route path needs to be / not /tips
+
 // This API route is a GET Route for retrieving all the tips
-router.get('/tips', (req, res) => {
+router.get('/', (req, res) => {
   console.info(`${req.method} request received for tips`);
   TipService.getTips().then((data) => res.json(data));
 });
 
+
+// FIX: since the middleware in the index is loading this POST route to 
+//  path /tips, the route path needs to be / not /tips
+
 // This API route is a POST Route for a new UX/UI tip
-router.post('/tips', async (req, res) => {
+router.post('/', async (req, res) => {
   const { username, topic, tip } = req.body;
   if (req.body) {
     await TipService.addTip(username, topic, tip);
