ecd8c8e149cb336bac3d30ba4b5daa9da0a6bd0b
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-13 16:06:10 -0800

    10.1 updates

--- lessons/10.1/02-Stu_PostgreSQL-Install/Unsolved/schema.sql
+++ lessons/10.1/02-Stu_PostgreSQL-Install/Unsolved/schema.sql
@@ -9,6 +9,11 @@ As a developer, I want to execute SQL statements from a file using the PostgreSQ
 Acceptance Criteria
 It's done when using the command psql -U postgres enters the PostgreSQL command-line interface.
 It's done when the schema.sql file is executed using the command-line interface and a message reading Query OK is returned.
+    psql -U postgres -f schema.sql
+-or-
+    From within the interactive console:
+    psql -U postgres
+    \i schema.sql
 
 📝 Notes
 Refer to the documentation:
@@ -17,9 +22,11 @@ PostgreSQL documentation on meta-commands such as executing SQL statements from 
 
 💡 Hints
 Which commands do you use to start PostgreSQL in the command-line interface?
+    psql -U postgres
 Which meta-command can you use to quit out of the PostgreSQL command-line interface?
+    \i schema.sql
 
-You may need to change the file encoding for windows:
+You may need to change the file encoding for Windows:
 Get-Content .\schema-utf8.sql | Set-Content -Encoding utf8 schema-utf8.sql
 */
 
