933f04751ce85412e4a0848999b7bb4b414bcd9a
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-17 15:37:31 -0800

    10.3 exercizes, miniproject remains todo

--- lessons/10.3/26-Stu_COUNT-GROUPBY/Unsolved/src/server.ts
+++ lessons/10.3/26-Stu_COUNT-GROUPBY/Unsolved/src/server.ts
@@ -1,6 +1,7 @@
 /*
 
 📐 Add Comments to Implementation of Aggregate Functions in PostgreSQL Queries
+
 Work with a partner to add comments describing the functionality of the code 
   found in server.
 
@@ -13,6 +14,13 @@ If you have completed this activity, work through the following challenge with
   your partner to further your knowledge:
 What other aggregate functions can we use in PostgreSQL queries?
 
+ARRAY_AGG(): Returns an array of values.
+STRING_AGG(): Concatenates values into a single string with a specified delimiter.
+BOOL_AND(): Returns true if all input values are true.
+BOOL_OR(): Returns true if at least one input value is true.
+JSON_AGG(): Aggregates values into a JSON array.
+
+ *
  */
 
 import express from 'express';
@@ -29,6 +37,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // TODO: Write a comment to explain what the following code is doing.
+/* 
+ * Run the SELECT query using an available connection from the pool.
+
+ * The query returns the records matching a list of favorites grouped by whether
+ * the title is in stock.
+ * 
+ * If there is an error running the query the details of the error are logged
+ * to the console.
+ * 
+ * If the query is successful the matching rows are returned
+ */
 pool.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', (err: Error, result: QueryResult) => {
   if (err) {
     console.log(err);
@@ -38,6 +57,20 @@ pool.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stoc
 });
 
 // TODO: Write a comment to explain what the following code is doing.
+/* 
+ * Run the SELECT query using an available connection from the pool.
+ * 
+ * Queries a table of favorite books and returns sets of data for each section:
+ * - total_in_section: the total number of favorite book items split out by each section
+ * - max_quantity: the book with the highest quantity in stock
+ * - min_quantity: the book with the lowest quantity in stock
+ * - avg_quantity: the average quantity of books in stock for that section
+ * 
+ * If there is an error running the query the details of the error are logged
+ * to the console.
+ * 
+ * If the query is successful the matching rows are returned
+ */
 pool.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', (err: Error, result: QueryResult) => {
   if (err) {
     console.log(err);
