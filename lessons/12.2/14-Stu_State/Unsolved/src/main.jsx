4d64629e05d6f1f7267e252c27d857a99e4df85f
Clint Jones <clinton.alan.jones@gmail.com>
2025-01-09 12:07:09 -0800

    Prepped Mod 12 day 2 files

--- lessons/12.2/14-Stu_State/Unsolved/src/main.jsx
+++ lessons/12.2/14-Stu_State/Unsolved/src/main.jsx
@@ -1,3 +1,24 @@
+/*
+ * 📐 Add Comments to Implementation of Event Handlers
+ * 
+ * Activity
+ * Work with a partner to add comments describing the functionality of the code found 
+ *  in the components folder.
+ * 
+ * 📝 Notes
+ * Refer to the documentation:
+ * React Docs on useState Hook
+ * React Docs on handling events
+ * 
+ * 🏆 Bonus
+ * If you have completed this activity, work through the following challenge with 
+ *  your partner to further your knowledge:
+ * Q: What could be a benefit of working with multiple useState hooks within a single 
+ *  component?
+ * A: TODO
+ * 
+ */
+
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- lessons/12.2/18-Stu_Hooks-useEffect/Unsolved/src/main.jsx
+++ lessons/12.2/18-Stu_Hooks-useEffect/Unsolved/src/main.jsx
@@ -1,3 +1,30 @@
+/*
+ * 📖 Implement the useEffect Hook
+ * 
+ * Activity
+ * Work with a partner to implement the following user story:
+ * 
+ * As a developer, I want to be able to use the useEffect Hook to update the browser 
+ *  tab to reflect the current temperature.
+ * 
+ * Acceptance Criteria
+ * It's done when I have imported useEffect into Thermostat.jsx.
+ * It's done when I have set a value for the temp variable.
+ * It's done when I have used the useEffect Hook to set the document.title to the 
+ *  current temperature.
+ * 
+ * 📝 Notes
+ * Refer to the documentation:
+ * React Docs on the Effect Hook
+ * 
+ * 🏆 Bonus
+ * If you have completed this activity, work through the following challenge with your 
+ *  partner to further your knowledge:
+ * Q: What are some other use cases for the useEffect() Hook?
+ * A: TODO
+ * 
+ */
+
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
