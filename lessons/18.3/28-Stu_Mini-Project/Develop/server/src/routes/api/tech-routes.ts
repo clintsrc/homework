23410b71fbb588ecdcf210449f694ee79a45f3c4
Clint Jones <clinton.alan.jones@gmail.com>
2025-02-25 09:22:48 -0800

    Very old revisions - replacing them

--- lessons/18.3/28-Stu_Mini-Project/Develop/server/src/routes/api/tech-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-import { getAllTech } from '../../controllers/tech-controller.js';
-
-router.route('/').get(getAllTech);
-
-export default router;
--- lessons/18.3/28-Stu_Mini-Project/Develop/server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import apiRoutes from './api/index.js';
-
-router.use('/api', apiRoutes);
-
-export default router;
