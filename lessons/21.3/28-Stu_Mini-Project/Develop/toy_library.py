957bc97e4644e4c9a5570221bc647353cf77772b
Clint Jones <clinton.alan.jones@gmail.com>
2025-03-19 12:08:32 -0700

    Rearranged the pseudocode

--- lessons/21.3/28-Stu_Mini-Project/Develop/toy_library.py
+++ lessons/21.3/28-Stu_Mini-Project/Develop/toy_library.py
@@ -1,84 +1,3 @@
-"""
-Pseudocode from reqs:
-
-checkout_toy():
-   Call print_library(optional status="Available")
-        prints available items
-   Updates an item in the toy_library: 
-        if available
-            status == "Checked Out,"
-            update checkout_date and due_date values
-            return True
-        if "Checked Out":
-            (Do not update the toy)
-            return False
-
-return_toy():
-    Call the print_library(optional status="Checked Out")
-        print checked out items
-    if checked_out
-        Change the status to "Available," updates the values of checkout_date and due_date to empty strings, and returns True. 
-    if status=="Available"
-        (Do not update)
-        return False
-
-add_toy(name, toy_type="", status="Available", replacement_cost=0.0):
-    Add a new toy
-
-remove_toy():
-    Remove a toy
-    Use listname.pop() to return the toy (dictionary) that was removed.
-
-print_library(status=None, view_details=False):
-    status="Availble"
-        list comprehension filter toy_library by status
-            Loop through the (filtered) toy library
-            print the index, name of the toy, and its status
-    view_details=True
-        print the remaining dictionary values
-
-check_if_index_in_library()
-    # called when a user types in a value to represent an index in the toy_library
-    if input can be integer
-    then
-        convert input to integer
-        if input is a valid index, len(toy_library)
-            return input as an integer
-        else
-            return None
-
-MAIN
-* Pressing "Enter" at the first prompt returns to the main menu instead of 
-selecting an option
-* Provide an option to return to the main menu from each of these options:
-checkout_toy
-return_toy
-add_toy
-remove_toy
-
-Menu: "View the library"
-    print_library()
-        for each item (all items) in the toy_library print:
-            index
-            toy name
-            status
-
-Menu: "View the detailed library"
-    print_library(view_details=True)
-        for each item (all items) in the toy_library print:
-            index
-            toy name
-            status
-            if view_details:
-                toy type
-                checkout count
-                and replacement cost
-                if status == "Checked Out":
-                    checked out date
-                    return date
-"""
-
-
 """This script represents a toy library"""
 
 # Use the standard library for the checkout timestamps
@@ -127,18 +46,39 @@ toy_library = [
 # TODO: Define the checkout function
 def checkout_toy():
     print("checkout_toy - TODO")
+#    Call print_library(optional status="Available")
+#         prints available items
+#    Updates an item in the toy_library: 
+#         if available
+#             status == "Checked Out,"
+#             update checkout_date and due_date values
+#             return True
+#         if "Checked Out":
+#             (Do not update the toy)
+#             return False
 
 # TODO: Define the return_toy function
 def return_toy():
     print("return_toy - TODO")
+    # Call the print_library(optional status="Checked Out")
+    #     print checked out items
+    # if checked_out
+    #     Change the status to "Available," updates the values of checkout_date and due_date to empty strings, and returns True. 
+    # if status=="Available"
+    #     (Do not update)
+    #     return False
 
 # TODO: Define the add_toy function
 def add_toy():
     print("add_toy - TODO")
+# add_toy(name, toy_type="", status="Available", replacement_cost=0.0):
+#     Add a new toy
 
 # TODO: Define the remove_toy function
 def remove_toy():
     print("remove_toy - TODO")
+    # Remove a toy
+    # Use listname.pop() to return the toy (dictionary) that was removed.
 
 # TODO: Define the print library function
 def print_library(status=None, view_details=False):
@@ -153,13 +93,55 @@ def print_library(status=None, view_details=False):
         print(f"{index + 1}. {toy['toy']}: {toy['status']}")
         if view_details:
             print("TODO")
+# print_library(status=None, view_details=False):
+#     status="Availble"
+#         list comprehension filter toy_library by status
+#             Loop through the (filtered) toy library
+#             print the index, name of the toy, and its status
+#     view_details=True
+#         print the remaining dictionary values
+# ##### View Library #####
+    # print_library()
+    #     for each item (all items) in the toy_library print:
+    #         index
+    #         toy name
+    #         status
+# ##### View DETAILED Library #####
+    # print_library(view_details=True)
+    #     for each item (all items) in the toy_library print:
+    #         index
+    #         toy name
+    #         status
+    #         if view_details:
+    #             toy type
+    #             checkout count
+    #             and replacement cost
+    #             if status == "Checked Out":
+    #                 checked out date
+    #                 return date    
 
 # TODO: Define a function to check if the user input is in the toy library
+# called when a user types in a value to represent an index in the toy_library
 def check_if_index_in_library():
     print("check_if_index_in_library - TODO")
-
+    # if input can be integer
+    # then
+    #     convert input to integer
+    #     if input is a valid index, len(toy_library)
+    #         return input as an integer
+    #     else
+    #         return None
 
 # Define the main function
+# TODO:
+# MAIN
+# * Pressing "Enter" at the first prompt returns to the main menu instead of 
+# selecting an option
+# * Provide an option to return to the main menu from each of these options:
+# checkout_toy
+# return_toy
+# add_toy
+# remove_toy
 if __name__ == "__main__":
 
     library_menu = {
